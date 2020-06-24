39143ebce50c6af1c2f4fbccb1d3f11f181ba7be
Ron Benchetrit <ronbenc@campus.technion.ac.il>
2020-06-24 20:02:55 +0300

    Create Charcter

---
+++ Character.h
@@ -0,0 +1,19 @@
+#include "Auxiliaries.h"
+
+namespace mtm 
+{
+    class Character
+    {
+    private: //private with access functions or protected?
+        GridPoint position(); //should be a field in Character?
+        unsigned int health;
+        unsigned int ammo;
+        unsigned int range;
+        unsigned int power;
+        unsigned int move_range;//moveset?
+    public:
+        Character(int health, int ammo, int range, int power): health(health), ammo(ammo), range(range), power(power){};
+        virtual ~Character();
+    };
+    
+}
